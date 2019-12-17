c6b99d9652366d7a6b866091dc8c298e5d2f1e81
Runkai Zhang <1105420698@qq.com>
2019-12-17 15:20:18 -0500

    Initial Commit

---
+++ Choate Tour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Choate Tour
+//
+//  Created by Runkai Zhang on 12/17/19.
+//  Copyright © 2019 Runkai Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
