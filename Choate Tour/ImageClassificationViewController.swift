5d8a985e94b89fa49cbfa5d85e5c4e8cfd8dc6ea
Runkai Zhang <1105420698@qq.com>
2019-12-27 19:08:20 -0500

    Shrinks project by combining files.

--- Choate Tour/ImageClassificationViewController.swift
+++ Choate Tour/ImageClassificationViewController.swift
@@ -296,3 +296,20 @@ fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [U
 fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
 	return input.rawValue
 }
+
+extension CGImagePropertyOrientation {
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            fatalError()
+        }
+    }
+}
